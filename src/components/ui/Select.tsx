683d8fdaf6a888707e8d12aaf2ea50ca27ebbe8c
Tousif Dewan <96633212+Tousif2021@users.noreply.github.com>
2025-06-07 13:56:57 +0200

    Updated Select.tsx

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -131,7 +131,7 @@ export const Select: React.FC<SelectProps> = ({
             {...props}
           >
             {placeholder && (
-              <option value="" disabled hidden>
+              <option value="\" disabled hidden>
                 {placeholder}
               </option>
             )}
