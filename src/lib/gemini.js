035f8dec31d0689fa9e7c56b958601b3dd1830ed
Tousif Dewan <96633212+Tousif2021@users.noreply.github.com>
2025-06-10 22:14:39 +0200

    Updated gemini.js

--- src/lib/gemini.js
+++ src/lib/gemini.js
@@ -1,8 +1,8 @@
-import { GoogleGenerativeAI } from '@google/generative-ai';
+const { GoogleGenerativeAI } = require('@google/generative-ai');
 
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY!;
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 if (!GEMINI_API_KEY) throw new Error('Gemini API key not set!');
 
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
 
-export default genAI;
+module.exports = genAI;
