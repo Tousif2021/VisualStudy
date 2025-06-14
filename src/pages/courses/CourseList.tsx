5976c9cbe7fc40ecb5d8d07bb617f085c2827d6b
Tousif Dewan <96633212+Tousif2021@users.noreply.github.com>
2025-06-14 11:56:00 +0200

    Updated CourseList.tsx

--- src/pages/courses/CourseList.tsx
+++ src/pages/courses/CourseList.tsx
@@ -75,13 +75,14 @@ export const CourseList: React.FC = () => {
       relative flex flex-col justify-center items-center
     min-h-[220px] aspect-square
     rounded-[1.5rem]
-    bg-transparent
-    border border-blue-200
-    shadow-[0_0_20px_6px_rgba(59,130,246,0.15)]
-    hover:shadow-[0_0_28px_10px_rgba(59,130,246,0.25)]
+    bg-white
+    border border-transparent
+    shadow-[0_0_50px_12px_rgba(59,130,246,0.35)]
+    hover:shadow-[0_0_70px_20px_rgba(59,130,246,0.45)]
     transition-all duration-300 ease-in-out
     cursor-pointer
     group
+    overflow-hidden
     `}
   >
     {/* Centered Big Initials */}
